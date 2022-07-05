3d668a5ffe83bb80c24e77dde64a3af82df8f216
Alejandro Paz <alepazte@hotmail.com>
2022-07-04 19:57:20 -0600

    feat: add calendar elements

---
+++ src/Calendar/Calendar.tsx
@@ -0,0 +1,73 @@
+import { useMemo, useState } from "react";
+import "./Calendar.css";
+
+type CalendarProps = {
+  date: Date;
+};
+
+export function Calendar({ date }: CalendarProps) {
+  const daysInMonth = useMemo(
+    () => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(),
+    [date.getMonth(), date.getFullYear()]
+  );
+
+  console.log("daysInMonth", daysInMonth);
+
+  return (
+    <div className="calendarContainer">
+      <div className="weekdays">
+        <div>Sun</div>
+        <div>Mon</div>
+        <div>Tue</div>
+        <div>Wed</div>
+        <div>Thu</div>
+        <div>Fri</div>
+        <div>Sat</div>
+      </div>
+      <div className="days">
+        <div>26</div>
+        <div>27</div>
+        <div>28</div>
+        <div>29</div>
+        <div>30</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+        <div>13</div>
+        <div>14</div>
+        <div>15</div>
+        <div>16</div>
+        <div>17</div>
+        <div>18</div>
+        <div>19</div>
+        <div>20</div>
+        <div>21</div>
+        <div>22</div>
+        <div>23</div>
+        <div>24</div>
+        <div>25</div>
+        <div>26</div>
+        <div>27</div>
+        <div>28</div>
+        <div>29</div>
+        <div>30</div>
+        <div>31</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+    </div>
+  );
+}
--- src/CalendarView/CalendarView.tsx
+++ src/CalendarView/CalendarView.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { monthName } from "../utils/utils";
 import { CalendarHeader } from "../CalendarHeader";
+import { Calendar } from "Calendar/";
 
 export function CalendarView() {
   //Date to display
@@ -11,7 +12,7 @@ export function CalendarView() {
       {/* Calendar header and controls */}
       <CalendarHeader date={date} setDate={setDate} />
       {/*Calendar Section*/}
-
+      <Calendar date={date} />
     </div>
   );
 }
